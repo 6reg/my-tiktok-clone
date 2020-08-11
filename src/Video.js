061ede73e7c71caacc2ede1ecc096aa326c0a70e
greg mathias <mathiasgreg@gmail.com>
2020-08-11 14:52:05 -0400

    7 changes

---
+++ src/Video.js
@@ -0,0 +1,30 @@
+import React, { useRef, useState } from 'react'
+import "./Video.css";
+function Video() {
+    const [playing, setPlaying] = useState(false);
+    const videoRef = useRef(null);
+
+    const onVideoPress = () => {
+        if (playing) {
+            videoRef.current.pause();
+            setPlaying(false);
+        } else {
+            videoRef.current.play();
+            setPlaying(true);
+        }
+    };
+
+    return (
+        <div className="video">
+           <video className="video__player" 
+           loop
+           onClick={onVideoPress}
+           ref={videoRef}
+           src="https://www.tiktok.com/@jasoncoffee/video/6850225904753429766"></video>
+            {/* <VideoFooter /> */}
+            {/* <VideoSidebar /> /> */}
+        </div>
+    );
+}
+
+export default Video
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC-_4P_crefiiBYEiVkem_mGw0PALu2Tdo",
+    authDomain: "tik-tok-clone-e8ef5.firebaseapp.com",
+    databaseURL: "https://tik-tok-clone-e8ef5.firebaseio.com",
+    projectId: "tik-tok-clone-e8ef5",
+    storageBucket: "tik-tok-clone-e8ef5.appspot.com",
+    messagingSenderId: "564597002338",
+    appId: "1:564597002338:web:baef8ab0bbc9775bd8813b",
+    measurementId: "G-DDRNJLXC0X"
+  };
